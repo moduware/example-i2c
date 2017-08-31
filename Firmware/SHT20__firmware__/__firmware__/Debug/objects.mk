503dcbef290323ff33c2c6f593627b355af471b6
balli21nov <balli21nov@gmail.com>
2017-08-31 22:01:02 +1000

    Firmware updated with I2C library

---
+++ Firmware/SHT20__firmware__/__firmware__/Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -l"C:/D\ DRIVE/NEXPAQ/nexpaq/Example-i2c/Firmware/SHT20__firmware__/__firmware__/mdk/np_module_mdk_v1.lib" -l"libc.a"
+
